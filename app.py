2113650b540cd3f50aa61dfcdfe0189660a5f1cd
LeslieMills <59105912+LeslieMills@users.noreply.github.com>
2025-08-05 11:50:26 +0000

    Updated home page with rotating banner.

--- app.py
+++ app.py
@@ -2,10 +2,27 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+PRODUCT = [{
+    'id': 1,
+    'product': 'Hardware',
+    'Name': 'Ardean Drone',
+    'Description': 'N/A'
+}, {
+    'id': 2,
+    'product': 'Software',
+    'Name': 'QuMapp',
+    'Description': 'N/A'
+}]
+
 
 @app.route('/')
 def homepage():
   return render_template('home.html')
+
+
+@app.route('/about')
+def product_page():
+  return render_template('product.html', products=PRODUCT)
 
 
 if __name__ == '__main__':
